969ac00762aa21090c5393c1ad2a18da843f2a9e
U-BSD\519819 <519819@SHS-GYBF6X3.beaverton.k12.or.us>
2025-11-18 10:54:40 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+#include <map>
+#include "room.h"
+using namespace std;
+
+// main function
+int main(){
+  return 0;
+}
---
+++ room.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cstring>
+#include <map>
+#include "room.h"
+
+room::room(){}

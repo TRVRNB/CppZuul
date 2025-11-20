187c1fb560fba78dfc621765496afe3de941059b
TRVRNB <travis.renberg@gmail.com>
2025-11-20 09:39:12 -0800

    Add files via upload

--- room.h
+++ room.h
@@ -11,19 +11,13 @@ class room {
   // EAST: 2
   // SOUTH: 3
   // WEST: 4
-  int id; // this will be automatically set in main(), to be unique from all others
-  char special[81] = "NONE"; // change this to apply special logic to the room in main()
-  char name[81] = "ROOM"; // room name that is displayed
-  char description[401] = "ROOM DESCRIPTION"; // this appears if you use the SEARCH command
-  char detailed_description[401] = "DETAILED ROOM DESCRIPTION"; // appears if room search is successful
-  int difficulty = 7; // the minimum roll needed to succeed a search
-  bool extra_detail = false; // not all rooms have something special to be discovered
+  char name[81] = "room"; // room name that is displayed, used as an "identifier"
+  char description[401] = "You enter a room."; // this appears upon room entry
+  char detailed_description[401] = "There is nothing noteworthy in this room."; // appears if SEARCH command is used
   char item_simple[81] = "NONE"; // use the PICK command to get items in a room
   char item_detailed[81] = "NONE"; // this requires the search check to succeed
   room(); // the variables will be defined in main() rather than through a constructor
  private:
-  bool detailed_search = false; // update to true if the player successfully searches
-  
 };
 
 
